b9f79861ca9afdd79628186a632870adff474ebb
Myron van Leuveren <myron.van.leuveren@car-go.nl>
2023-10-19 23:26:51 +0200

    Added sleep command.

--- stdcommands/sleep/sleep.go
+++ stdcommands/sleep/sleep.go
@@ -1,21 +1,47 @@
 package sleep
 
 import (
-	"time"
-
+	"fmt"
+	"github.com/cirelion/flint/bot"
 	"github.com/cirelion/flint/commands"
+	"github.com/cirelion/flint/common"
+	"github.com/cirelion/flint/common/scheduledevents2"
 	"github.com/cirelion/flint/lib/dcmd"
-	"github.com/cirelion/flint/stdcommands/util"
+	"github.com/cirelion/flint/moderation"
+	"time"
 )
 
 var Command = &commands.YAGCommand{
-	CmdCategory:          commands.CategoryDebug,
-	HideFromCommandsPage: true,
-	Name:                 "sleep",
-	Description:          "Maintenance command, used to test command queueing. Bot Owner Only",
-	HideFromHelp:         true,
-	RunFunc: util.RequireOwner(func(data *dcmd.Data) (interface{}, error) {
-		time.Sleep(time.Second * 5)
-		return "Slept, Done", nil
-	}),
+	Name:         "sleep",
+	Description:  "Mutes you so you can sleep in peace. Default is 6 hours.",
+	CmdCategory:  commands.CategoryGeneral,
+	RequiredArgs: 0,
+	Arguments: []*dcmd.ArgDef{
+		{Name: "Duration", Type: &commands.DurationArg{}, Default: time.Hour * 6},
+	},
+	ApplicationCommandEnabled: true,
+	RunFunc: func(data *dcmd.Data) (interface{}, error) {
+		guildID := data.GuildData.GS.ID
+		config, err := moderation.GetConfig(guildID)
+		duration := data.Args[0].Value.(time.Duration)
+		member, err := bot.GetMember(guildID, data.Author.ID)
+		if err != nil {
+			return nil, err
+		}
+
+		err = common.AddRoleDS(member, config.IntMuteRole())
+		if err != nil {
+			return nil, err
+		}
+
+		err = scheduledevents2.ScheduleRemoveRole(data.Context(), data.GuildData.GS.ID, data.Author.ID, config.IntMuteRole(), time.Now().Add(duration))
+		if err != nil {
+			return nil, err
+		}
+
+		// cancel the event to add the role
+		scheduledevents2.CancelAddRole(data.Context(), data.GuildData.GS.ID, data.Author.ID, config.IntMuteRole())
+
+		return fmt.Sprintf("Sweet dreams %s, see you in %s!", data.Author.Mention(), common.HumanizeDuration(common.DurationPrecisionMinutes, duration)), nil
+	},
 }
